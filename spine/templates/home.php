816a51a79963f587e122dcb6f5abb9b0675ddc25
Matt Barba <matt@placester.com>
2011-12-06 08:45:12 -0500

    minor updates.
    Signed-off-by: Matt Barba <matt@placester.com>

--- spine/templates/home.php
+++ spine/templates/home.php
@@ -9,10 +9,8 @@
  */
 ?>
 <div id="slideshow" class="clearfix theme-default left bottomborder grid_8 alpha">
-    <?php //echo PLS_Slideshow::slideshow( array( 'anim_speed' => 1000, 'pause_time' => 15000, 'control_nav' => true, 'width' => 620, 'height' => 300, 'context' => 'home', 'listings' => 'limit=6&is_new=true&sort_by=price' ) ); ?>
+    <?php echo PLS_Slideshow::slideshow( array( 'anim_speed' => 1000, 'pause_time' => 15000, 'control_nav' => true, 'width' => 620, 'height' => 300, 'context' => 'home', 'listings' => 'limit=6&is_new=true&sort_by=price' ) ); ?>
 </div>
 <div id="listing" class="grid_8 alpha">
-	<h2>Featured Listing List</h2>
-	<p> This will list out the details of up to 5 listings marked as "featured" in the listings tab of the Placester Plugin</p>
     <?php echo pls_get_listings( "limit=5&is_featured=true&context=home" ) ?>
 </div>
