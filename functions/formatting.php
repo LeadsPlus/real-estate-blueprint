d4eebc0848dfa8312fe2f8a4de4a49ec14c132db
Matt Barba <matt@placester.com>
2011-12-19 10:56:28 -0500

    added basic phone number formatting.
    Signed-off-by: Matt Barba <matt@placester.com>

--- functions/formatting.php
+++ functions/formatting.php
@@ -3,6 +3,47 @@
 class PLS_Format {
 
 
+	static public function phone ($phone, $options = '') {
+		$new_phone = '';
+
+		/** Define the default argument array. */
+        $defaults = array(
+        	'format' => 'hyphens',
+        	'html_args' => array()
+	        );
+
+        /** Merge the arguments with the defaults. */
+        $options = wp_parse_args( $options, $defaults );
+
+        extract( $options, EXTR_SKIP );
+
+        if (self::validate_number(&$phone)) {
+            $phone_parts = self::process_phone_parts($phone);
+
+            switch ($format) {
+            	case 'hyphens':
+            		$new_phone = self::format_phone($phone_parts, '-');
+            		break;
+
+            	case 'spaces':
+					$new_phone = self::format_phone($phone_parts, ' ');
+            		break;
+
+            	case 'as_html':
+            		$new_phone = self::format_phone_html($phone_parts, $html_args);
+            		break;
+            	
+            	default:
+            		# code...
+            		break;
+            }
+        
+    	}
+
+        return $new_phone;
+
+	}
+
 	// formats the given number based on the supplied options. 
 	static public function number ( $number, $options = '') {
 		
@@ -69,6 +110,104 @@ class PLS_Format {
 
 		return $abbreviated_number;
 	}
+
+	static private function validate_number ($phone) {
+		
+		//placester api dumps a + in there. 		
+		if (substr($phone, 0, 1) == '+') {
+			$phone = substr($phone, 1);
+		}
+
+		
+		if (is_numeric($phone)) {
+		
+			// test if there's a 1 prepended.
+			if (strlen($phone) === 11 && $phone[0] == 1) {
+				//pop 1 off. 
+				$phone = substr($phone, 1);
+				return true;
+			
+			} elseif (strlen($phone) === 10) {
+				// it's a 10 digit number... I guess that works.
+				return true;	
+			}
+		}
+
+		// is invalid if no return by here. 
+		return false;
+	}
+
+	static private function process_phone_parts ($phone) {
+    	
+    	$phone_parts = array();
+    	//area code in states
+    	$phone_parts[] = substr($phone, 0,3);	
+    	//next 3
+    	$phone_parts[] = substr($phone, 3, 3);	
+    	// next 4
+    	$phone_parts[] = substr($phone, 6,4);	
+
+    	return $phone_parts;
+	}
+
+	static private function format_phone ($phone_parts, $delimiter = '') {
+		
+		$new_phone = '';
+		
+		foreach ($phone_parts as $key => $part) {
+			if ($key == 0) {
+				$new_phone .= $part;
+			} else {
+				$new_phone .= $delimiter . $part;	
+			}
+		}
+
+		return $new_phone;
+	}
+
+	static private function format_phone_html ($phone_parts, $args) {
+		$new_phone = '';
+
+		/** Define the default argument array. */
+        $defaults = array(
+        	'wrapper' => '',
+        	'area_wrapper' => '',
+        	'three_wrapper' => '',
+        	'four_wrapper' => '',
+        	'delimiter' => ' '
+	        );
+
+        /** Merge the arguments with the defaults. */
+        $options = wp_parse_args( $args, $defaults );
+
+        extract( $options, EXTR_SKIP );
+
+		if ($area_wrapper != '') {
+			$new_phone .= pls_h($area_wrapper, $phone_parts[0]);
+		} else {
+			$new_phone .= $phone_parts[0];
+		}
+
+		if ($three_wrapper != '') {
+			$new_phone .= pls_h($three_wrapper, $phone_parts[1]);
+		} else {
+			$new_phone .= $delimiter . $phone_parts[1];
+		}
+
+		if ($four_wrapper != '') {
+			$new_phone .= pls_h($four_wrapper, $phone_parts[2]);
+		} else {
+			$new_phone .= $delimiter . $phone_parts[2];
+		}
+
+		if ($wrapper != '') {
+			$new_phone = pls_h($wrapper, $new_phone);	
+		}
+
+		return $new_phone;
+
+	}
+
 
 //end of class
 }
