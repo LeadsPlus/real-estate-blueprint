04dda46d7489087e2a131b1c7c0fdf2f0b95d380
Matt Barba <matt@placester.com>
2011-12-19 09:41:42 -0500

    quick fix for now. Will need to settle on a standard for how this is used in the future.
    Signed-off-by: Matt Barba <matt@placester.com>

--- extensions/image-util/fancybox/default-settings.js
+++ extensions/image-util/fancybox/default-settings.js
@@ -1,6 +1,18 @@
 $(document).ajaxStop(function() {
-	/* Apply fancybox to multiple items */
 	
+	/* Apply fancybox to multiple items */
+	$("a.pls_use_fancy").fancybox({
+		'transitionIn'	:	'elastic',
+		'transitionOut'	:	'elastic',
+		'speedIn'		:	600, 
+		'speedOut'		:	200, 
+		'overlayShow'	:	false
+	});
+	
+});
+$(document).ready(function() {
+	
+	/* Apply fancybox to multiple items */
 	$("a.pls_use_fancy").fancybox({
 		'transitionIn'	:	'elastic',
 		'transitionOut'	:	'elastic',
