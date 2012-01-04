30ef1ebae589b246be0078a0656b699cd561f647
Matt Barba <matt@placester.com>
2012-01-03 21:43:10 -0500

    updated listings widget styles.
    Signed-off-by: Matt Barba <matt@placester.com>

--- partials/custom-listings-widget.php
+++ partials/custom-listings-widget.php
@@ -6,7 +6,7 @@ function custom_widget_html_filter ($listing_html, $listing_data) {
     ?>
 		<section class="listing-item">
 			<h4>
-				<a href="<?php echo $listing_data->url; ?>"><?php echo $listing_data->location->full_address ?></a>
+				<a href="<?php echo $listing_data->url; ?>"><?php echo $listing_data->location->address ?></a>
 			</h4>
 			<section class="details">
 			    <span class="bed"><?php echo $listing_data->bedrooms; ?> Beds</span>
@@ -16,7 +16,7 @@ function custom_widget_html_filter ($listing_html, $listing_data) {
 			<section class="featured-image">
 				<?php if ( isset($listing_data->images) && is_array($listing_data->images) ): ?>
 					<a href="<?php echo $listing_data->url ?>">
-			  		<?php echo PLS_Image::load($listing_data->images[0]->url, array('resize' => array('w' => 250, 'h' => 150, 'method' => 'crop'), 'fancybox' => true)); ?>
+			  		<?php echo PLS_Image::load($listing_data->images[0]->url, array('resize' => array('w' => 280, 'h' => 170, 'method' => 'crop'), 'fancybox' => true)); ?>
 					</a>
 				<?php endif ?>
 			</section>
