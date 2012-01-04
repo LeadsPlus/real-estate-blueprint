fe74f0a294357b872c43621229dbfa7e60f88497
Matt Barba <matt@placester.com>
2012-01-04 15:06:26 -0500

    question widget should be at the top for property details pages.
    Signed-off-by: Matt Barba <matt@placester.com>

--- templates/default-sidebar.php
+++ templates/default-sidebar.php
@@ -1,20 +1,18 @@
 <?php 
-	
-	// Quick Search Widget
-	$instance = array("title" => "Quick Search", "number" => 9);
-	$args = array();
-	$sb = new PLS_Quick_Search_Widget();
-	$sb->number = $instance['number'];
-	$sb->widget($args,$instance);
 
-
-	// Lead Caputure Form
+    // Lead Caputure Form
 	$instance = array("title" => "Have an questions?", "number" => 9);
 	$args = array();
 	$sb = new Placester_Contact_Widget();
 	$sb->number = $instance['number'];
 	$sb->widget($args,$instance);
 
+	// Quick Search Widget
+	$instance = array("title" => "Quick Search", "number" => 9);
+	$args = array();
+	$sb = new PLS_Quick_Search_Widget();
+	$sb->number = $instance['number'];
+	$sb->widget($args,$instance);
 
 	// Agent Widget
 	$instance = array("title" => "Give us a call", "number" => 9);
