79c37b454b0b02437283e4e7832a3adfcc8933b6
Galen Vinter <galen.vinter@gmail.com>
2012-02-07 16:08:02 -0500

    cleaning a bit of theme options until the new options are ready

--- options/colors.php
+++ options/colors.php
@@ -5,25 +5,4 @@ PLS_Style::add(array(
     "type" => "heading"));
 
 
-PLS_Style::add(array(
-    "name" => "General",
-    "type" => "info",
-    "desc" => "The follow options apply to attributes across your site"));
 
-
-// Add single CSS option for change to site
-PLS_Style::add(array( 
-			// div title in Theme Options Menu
-			"name" =>  "Example Background",
-			// div descrition in Theme Options Menu
-			"desc" => "Change the background CSS.",
-			// div id in Theme Options Menu
-			"id" => "example_background",
-			//
-			"std" => $background_defaults,
-			// selector of targeted tag being changed
-			"selector" => "",
-			// Theme Options Tab (type) which holds option being changed.
-			// examples: text, textarea, radio, images, textbox, multicheck,
-			// color, upload, typography, background, info, heading
-			"type" => "background"));
