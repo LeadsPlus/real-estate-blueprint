c67271b81da4c682576cfec357f6ad377797f237
Galen Vinter <galen.vinter@gmail.com>
2012-02-10 14:35:56 -0500

    added options to slideshow

--- templates/home.php
+++ templates/home.php
@@ -15,21 +15,20 @@
 					array( 
 						'animation' => 'fade', 									// fade, horizontal-slide, vertical-slide, horizontal-push
 						'animationSpeed' => 800, 								// how fast animtions are
-						'timer' => true, 												// true or false to have the timer
+						'timer' => 'false',											// true or false to have the timer
+						'pauseOnHover' => true,									// if you hover pauses the slider
 						'advanceSpeed' => 4000,									// if timer is enabled, time between transitions 
+						'startClockOnMouseOut' => true,					// if clock should start on MouseOut
 						'startClockOnMouseOutAfter' => 1000,		// how long after MouseOut should the timer start again
 						'directionalNav' => true, 							// manual advancing directional navs
 						'captions' => true, 										// do you want captions?
 						'captionAnimation' => 'fade', 					// fade, slideOpen, none
 						'captionAnimationSpeed' => 800, 				// if so how quickly should they animate in
-						// 'bullets' => false,											// true or false to activate the bullet navigation
-						// 'bulletThumbs' => false,								// thumbnails for the bullets
-						// 'bulletThumbLocation' => '',						// location from this file where thumbs will be
-
+						'afterSlideChange' => 'function(){}',		// empty function
 						'width' => 620, 
 						'height' => 300, 
 						'context' => 'home', 
-						'listings' => 'limit=6&is_new=true&sort_by=price' 
+						'listings' => 'limit=5&is_featured=true&sort_by=price'
 					)
 			); 
 		?>
