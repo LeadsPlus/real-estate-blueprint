bb9d251e34866a9b8148054c5a4f138501f83d59
Matt Barba <matt@placester.com>
2012-02-24 13:11:48 -0500

    This too.
    Signed-off-by: Matt Barba <matt@placester.com>

--- partials/custom-listings-widget.php
+++ partials/custom-listings-widget.php
@@ -6,28 +6,24 @@ function custom_widget_html_filter ($listing_html, $listing_data) {
     ?>
 		<section class="listing-item">
 			<h4>
-				<a href="<?php echo $listing_data->url; ?>"><?php echo $listing_data->location->address ?></a>
+				<a href="<?php echo $listing_data['cur_data']['url']; ?>"><?php echo $listing_data['location']['address'] ?></a>
 			</h4>
 			<section class="details">
-			    <span class="bed"><?php echo $listing_data->bedrooms; ?> Beds</span>
-			    <span class="bath"><?php echo $listing_data->bathrooms ?> Baths</span>
-			    <span class="area">1300 Sq Ft.</span>
+			    <span class="bed"><?php echo $listing_data['cur_data']['beds']; ?> Beds</span>
+			    <span class="bath"><?php echo $listing_data['cur_data']['baths'] ?> Baths</span>
+			    <span class="area"><?php echo $listing_data['cur_data']['sqft'] ?></span>
 			</section>
 			<section class="featured-image">
-				<?php if ( isset($listing_data->images) && is_array($listing_data->images) ): ?>
-					<a href="<?php echo $listing_data->url ?>">
-			  		<?php echo PLS_Image::load($listing_data->images[0]->url, array('resize' => array('w' => 280, 'h' => 170, 'method' => 'crop'), 'fancybox' => true)); ?>
+				<?php if ( is_array($listing_data['images']) ): ?>
+					<a href="<?php echo $listing_data['cur_data']['url'] ?>">
+			  		<?php echo PLS_Image::load($listing_data['images'][0]['url'], array('resize' => array('w' => 280, 'h' => 170, 'method' => 'crop'), 'fancybox' => true)); ?>
 					</a>
 				<?php endif ?>
 			</section>
-			<a class="learn-more" href="<?php echo $listing_data->url;?>">Learn More</a>
+			<a class="learn-more" href="<?php echo $listing_data['cur_data']['url'];?>">Learn More</a>
 			<div class="clearfix"></div>
 		</section>
      <?php
-
      $listing_html = ob_get_clean();
-
      return $listing_html;
-
-
 }
