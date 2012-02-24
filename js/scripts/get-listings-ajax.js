73aefcb7d7e1c7f94d062fc5af4e483ea94e042b
Matt Barba <matt@placester.com>
2012-02-24 07:13:11 -0500

    updated map to remove markers as well as add them when needed
    Signed-off-by: Matt Barba <matt@placester.com>

--- js/scripts/get-listings-ajax.js
+++ js/scripts/get-listings-ajax.js
@@ -1,4 +1,5 @@
 $(document).ready(function($) {
+    var markers = [];
     var my_listings_datatable = $('#placester_listings_list').dataTable( {
         "bFilter": false,
         "bProcessing": true,
@@ -16,15 +17,20 @@ $(document).ready(function($) {
                 "data" : aoData,
                 "success" : function(ajax_response) {
                     if (ajax_response && ajax_response['aaData']) {
+                        for (var current_marker in markers) {
+                          markers[current_marker].setMap(null);
+                        }
+                        markers = [];
                         var bounds = new google.maps.LatLngBounds();
                         for (var listing in ajax_response['aaData']) {
                             var listing_json = ajax_response['aaData'][listing][1];
-                            var marker = new google.maps.Marker({
+                            marker = new google.maps.Marker({
                                 position: new google.maps.LatLng(listing_json['location']['coords'][0], listing_json['location']['coords'][1]),
                                 map: pls_google_map,
                             });
                             marker.setMap(pls_google_map);
                             bounds.extend(marker.getPosition());
+                            markers.push(marker);
                         }
                         pls_google_map.fitBounds(bounds);
                     };
@@ -37,14 +43,14 @@ $(document).ready(function($) {
     });
 
     // prevents default on search button
-    $('#pls_admin_my_listings').live('change', function(event) {
+    $('#pls_search_form, #sort_by, #sort_dir').live('change', function(event) {
         event.preventDefault();
         my_listings_datatable.fnDraw();
     });
 
     // parses search form and adds parameters to aoData
     function my_listings_search_params (aoData) {
-        $.each($('#pls_admin_my_listings:visible').serializeArray(), function(i, field) {
+        $.each($('#pls_search_form:visible, .sort_wrapper').serializeArray(), function(i, field) {
             aoData.push({"name" : field.name, "value" : field.value});
         });
         return aoData;
