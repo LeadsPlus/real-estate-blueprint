2d2af290c71a2dd765e51fdc62d26374495513eb
Galen Vinter <galen.vinter@gmail.com>
2012-03-16 13:01:32 -0400

    removed doubling up of classes when container/inner classes aren't set

--- functions/widgets/contact.php
+++ functions/widgets/contact.php
@@ -57,8 +57,8 @@ class Placester_Contact_Widget extends WP_Widget {
 				$fname_label = apply_filters('fname_label', empty($instance['fname_label']) ? 'First Name (required)' : $instance['fname_label']);
 				$lname_label = apply_filters('lname_label', empty($instance['lname_label']) ? 'Last Name (required)' : $instance['lname_label']);
 				$question_label = apply_filters('question_label', empty($instance['question_label']) ? 'Any questions for us?' : $instance['question_label']);
-				$container_class = apply_filters('container_class', empty($instance['container_class']) ? 'placester_contact' : $instance['container_class']);
-				$inner_class = apply_filters('inner_class', empty($instance['inner_class']) ? 'common-side-cont' : $instance['inner_class']);
+				$container_class = apply_filters('container_class', empty($instance['container_class']) ? '' : $instance['container_class']);
+				$inner_class = apply_filters('inner_class', empty($instance['inner_class']) ? '' : $instance['inner_class']);
 
         $modern = @$instance['modern'] ? 1 : 0;
         $template_url = get_bloginfo('template_url');
