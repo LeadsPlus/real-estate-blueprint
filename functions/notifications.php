9fdb3241743b61b9f64340299ba42b76cd8cede5
Matt Barba <matt@placester.com>
2012-04-05 14:59:02 -0400

    only display the install plugin warning when it's not actually installed.
    Signed-off-by: Matt Barba <matt@placester.com>

--- functions/notifications.php
+++ functions/notifications.php
@@ -8,11 +8,12 @@ class PLS_Notifications {
 	}
 
 	function no_plugin () {
-		ob_start();
-		?>
-			<div style="margin-top: 10px; border: 1px solid #E6DB55;" class="update-nag">You are currently using a Placester theme without the <a href="http://wordpress.org/extend/plugins/placester/">Real Estate Website Builder</a> plugin. Please <a class="button" href="/wp-admin/plugin-install.php?tab=search&type=term&s=real+estate+website+builder&plugin-search-input=Search+Plugins">Install it Now</a></div>
-		<?php 
-		echo ob_get_clean();
+		if (pls_has_plugin_error()) {
+			ob_start();
+			?>
+				<div style="margin-top: 10px; border: 1px solid #E6DB55;" class="update-nag">You are currently using a Placester theme without the <a href="http://wordpress.org/extend/plugins/placester/">Real Estate Website Builder</a> plugin. This theme wont work without it. Please <a class="button" href="/wp-admin/plugin-install.php?tab=search&type=term&s=real+estate+website+builder&plugin-search-input=Search+Plugins">Install it Now</a></div>
+			<?php 
+			echo ob_get_clean();
+		}
 	}
-
 }
