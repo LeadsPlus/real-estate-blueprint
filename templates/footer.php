1e4a5cb90916cf9167bf2aaa811e2e11714ef310
Matt Barba <matt@placester.com>
2012-04-07 14:24:51 -0400

    move wp_footer right before body close.
    Signed-off-by: Matt Barba <matt@placester.com>

--- templates/footer.php
+++ templates/footer.php
@@ -8,37 +8,36 @@
  */
  ?>
 
-<?php pls_do_atomic( 'before_footer' ); ?>
+			<?php pls_do_atomic( 'before_footer' ); ?>
 
-<footer>
+			<footer>
 
-    <?php pls_do_atomic( 'open_footer' ); ?>
+			    <?php pls_do_atomic( 'open_footer' ); ?>
 
-    <div class="wrapper">
+			    <div class="wrapper">
 
-        <?php get_template_part( 'menu', 'subsidiary' ); ?>
+			        <?php get_template_part( 'menu', 'subsidiary' ); ?>
 
-        <?php pls_do_atomic( 'footer' ); ?>
+			        <?php pls_do_atomic( 'footer' ); ?>
 
-    </div><!-- .wrapper -->
+			    </div><!-- .wrapper -->
 
-    <?php pls_do_atomic( 'close_footer' ); ?>
+			    <?php pls_do_atomic( 'close_footer' ); ?>
 
-</footer>
+			</footer>
 
-<?php pls_do_atomic( 'after_footer' ); ?>
+			<?php pls_do_atomic( 'after_footer' ); ?>
 
-<?php wp_footer(); ?>
+		</div> <!-- #container -->
 
-</div> <!-- #container -->
+		<?php pls_do_atomic( 'close_body' ); ?>
 
-<?php pls_do_atomic( 'close_body' ); ?>
-
-<?php if (pls_get_option('pls-google-analytics')): ?>
-	<script type="text/javascript">
-		<?php echo pls_get_option('pls-google-analytics'); ?>
-	</script>
-<?php endif; ?>
-</body>
+		<?php if (pls_get_option('pls-google-analytics')): ?>
+			<script type="text/javascript">
+				<?php echo pls_get_option('pls-google-analytics'); ?>
+			</script>
+		<?php endif; ?>
+		<?php wp_footer(); ?>
+	</body>
 
 </html>
