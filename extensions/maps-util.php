856617a830f0524a436dc0549ddd426992208fb6
Matt Barba <matt@placester.com>
2012-05-10 18:03:44 -0400

    finishing touches on polygon search and basic styling for foundation.
    Signed-off-by: Matt Barba <matt@placester.com>

--- extensions/maps-util.php
+++ extensions/maps-util.php
@@ -272,68 +272,77 @@ class PLS_Map {
 						
 						var coords = [];
 						var request = {};
-						search_places();
+						
+						<?php if ($search_on_load): ?>
+							search_places();
+						<?php endif; ?>
 						
 						function search_places () {
-							get_lifestyle_form(function (new_point, request) {
-								request.location = new_point;
-								var service = new google.maps.places.PlacesService(<?php echo self::$map_js_var ?>.map);
-						        service.search(request, service_callback);	
-							}, function (new_point, request) {
-								var service = new google.maps.places.PlacesService(<?php echo self::$map_js_var ?>.map);
-						        service.search(request, service_callback);	
-							});
+							pls_show_loading_overlay();
+							var location = get_location();
+							console.log(location);
+							if (location && location.address) {
+								pls_geocode(location.address, <?php echo self::$map_js_var ?>, search_callback, function () {
+									search_callback(new google.maps.LatLng(<?php echo $lat; ?>, <?php echo $lng; ?>));
+								});		
+							} else {
+								search_callback(new google.maps.LatLng(<?php echo $lat; ?>, <?php echo $lng; ?>));
+							};
 						}
-				        
-				        function service_callback(results, status) {
-				        	var points = [];
-					        if (status == google.maps.places.PlacesServiceStatus.OK) {
-					        	for (var i = 0; i < results.length; i++) {						           
-									points.push({lat: results[i].geometry.location.lat(), lng: results[i].geometry.location.lng()});
-									pls_create_marker({latlng:results[i].geometry.location, content:results[i].name, icon: 'https://chart.googleapis.com/chart?chst=d_map_spin&chld=0.3|0|FF8429|13|b' }, <?php echo self::$map_js_var ?>)
-					          	}
-					        	var post_info = {}
-					        	post_info.action = 'find_hull';
-					        	post_info.points = points;
-					        	post_info.settings = {}
-					        	post_info.settings.include_listings = true;
-					        	$.post(info.ajaxurl, post_info, function(data, textStatus, xhr) {
-					        		if (data && data.polygon) {
-					        			pls_create_polygon(data.polygon,null, <?php echo self::$map_js_var ?>);
-					        		};
-					        		if (data.listings) {
-					        			for (var i = data.listings.length - 1; i >= 0; i--) {
-					        				pls_create_listing_marker(data.listings[i], <?php echo self::$map_js_var ?>);
-					        			};
-					        		};
-					        	}, 'json');
-					        }
-					      }
+						
+						function search_callback (new_point) {
+							request = {};
+							request.action = 'lifestyle_polygon';
+							request.location = {};
+							request.location = '' + new_point.lat() + ',' + new_point.lng();
+							request.radius = $('.location_select select#radius').val();
+							request.types = get_form_items();
+							$.post(info.ajaxurl, request, function(data, textStatus, xhr) {
+								var results = data.places;
+								if (results) {
+									for (var i = 0; i < results.length; i++) {						           
+										pls_create_marker({latlng: new google.maps.LatLng(results[i].geometry.location.lat, results[i].geometry.location.lng), content:results[i].name, icon: 'https://chart.googleapis.com/chart?chst=d_map_spin&chld=0.3|0|FF8429|13|b' }, <?php echo self::$map_js_var ?>)
+					          		}
+								};
+					          	if (data.polygon) {
+				        			pls_create_polygon(data.polygon,{strokeColor: '#55b429',strokeOpacity: 1.0,strokeWeight: 3, fillColor: 'c0ecac'}, <?php echo self::$map_js_var ?>);
+				        		};
+				        		if (data.listings) {
+				        			for (var i = data.listings.length - 1; i >= 0; i--) {
+				        				pls_create_listing_marker(data.listings[i], <?php echo self::$map_js_var ?>);
+				        			};
+				        		};
+				        		pls_hide_loading_overlay();
+							}, 'json');
+						}
 
-					      function get_lifestyle_form (success_callback, failed_callback) {
-					      	var response = {location: new google.maps.LatLng(<?php echo $lat; ?>, <?php echo $lng; ?>) , radius: 5000, types: ['school']};
-					      	var form_values = [];
+						function get_form_items() {
+							var response = [];
+							var form_values = [];
 					      	$.each($('#lifestyle_form_wrapper form').serializeArray(), function(i, field) {
-					      		console.log(field.name);
 								form_values.push(field.name);
 							});
 							if (form_values.length > 0) {
-								response.types = [];
+								response = [];
 								for (key in form_values) {
-									response.types.push(form_values[key]);
+									response.push(form_values[key]);
 								};
 							};
+							return response.join('|');
+						}
+				        
+						function get_location () {
+							var response = false;
 							var location_type = $('#lifestyle_form_wrapper select[name="location"]').val();
 							var location_value = $('.location_select_wrapper select#' + location_type).val();
 							if (location_value != 'Any') {
-								pls_geocode(location_value, <?php echo self::$map_js_var ?>, success_callback, failed_callback, response);	
-							} else {
-								failed_callback(null,response);
-							};
-					      	return response;
-					      }
+								response = {};
+								response.address = location_value;
+							} 
+							return response
+						}
 					      
-					      $('#lifestyle_form_wrapper form, .location_select_wrapper').live('change', function(event) {
+					      $('#lifestyle_form_wrapper form, .location_select_wrapper, .location_select').live('change', function(event) {
 					      	event.preventDefault();
 					      	pls_clear_markers(<?php echo self::$map_js_var ?>);
 					      	pls_clear_polygons(<?php echo self::$map_js_var ?>);
@@ -354,20 +363,27 @@ class PLS_Map {
 					});
 				});	  
 			</script>
-			<div class="<?php echo $class ?>" id="<?php echo $canvas_id ?>" style="width:<?php echo $width; ?>px; height:<?php echo $height; ?>px"></div>
-			
-			<section class="lifestyle_form_wrapper" id="lifestyle_form_wrapper">
-				<?php if ($show_lifestyle_controls): ?>
-					<div class="" >
-						<?php echo implode(self::get_area_selectors(), '') ?>
+			<div class="map_wrapper" style="position: relative">
+				<div id="loading_overlay" class="loading_overlay" style="z-index: 50; display: none; position: absolute; width:<?php echo $width; ?>px; height:<?php echo $height; ?>px"><?php echo $loading_overlay ?></div>
+				<div id="empty_overlay" class="empty_overlay" style="z-index: 50; display: none; position: absolute; width:<?php echo $width; ?>px; height:<?php echo $height; ?>px"><?php echo $empty_overlay ?></div>
+				<div class="<?php echo $class ?>" id="<?php echo $canvas_id ?>" style="width:<?php echo $width; ?>px; height:<?php echo $height; ?>px"></div>
+				<section class="lifestyle_form_wrapper" id="lifestyle_form_wrapper">
+					<?php if ($show_lifestyle_controls): ?>
+						<div class="location_wrapper" >
+							<?php echo implode(self::get_area_selectors(), '') ?>
+						</div>
+						<div class="clear"></div>
+					<?php endif ?>			
+					<div class="checkbox_wrapper">
+						<form>
+						<?php if ($show_lifestyle_checkboxes): ?>
+							<?php echo self::get_lifestyle_checkboxs(); ?>
+						<?php endif ?>
+						</form>
 					</div>
-				<?php endif ?>			
-				<form>
-					<?php if ($show_lifestyle_checkboxes): ?>
-						<?php echo self::get_lifestyle_checkboxs(); ?>
-					<?php endif ?>
-				</form>
-			</section>
+				</section>	
+			</div>
+			
 		<?php
 		return ob_get_clean();
 	}
@@ -379,27 +395,26 @@ class PLS_Map {
 	        $form_options['region'] = array_merge(array('false' => '---'), PLS_Plugin_API::get_location_list('region'));
 	        $form_options['postal'] = array_merge(array('false' => '---'),PLS_Plugin_API::get_location_list('postal')); 
 	        $form_options['neighborhood'] = array_merge(array('false' => '---'),PLS_Plugin_API::get_location_list('neighborhood')); 
-	        $response['location'] = '<select name="location" id="location" style="width: 140px">
-							<option value="locality">City</option>
-							<option value="region">State</option>
-							<option value="postal">Zip</option>
-							<option value="neighborhood">Neighborhood</option>
-						</select>';
+	        
+	        $response['radius'] = '<div class="location_select"><select name="radius" id="radius" style="width: 140px">
+							<option value="200">200 meters</option>
+							<option value="500">500 meters</option>
+							<option value="1000">1000 meters</option>
+							<option value="2000">2000 meters</option>
+							<option value="5000" selected>5000 meters</option>
+							<option value="10000">10000 meters</option>
+							<option value="20000">20000 meters</option>
+						</select></div>';
+	        $response['location'] = '<div class="location_select"><select name="location" id="location" style="width: 140px">
+				<option value="locality">City</option>
+				<option value="region">State</option>
+				<option value="postal">Zip</option>
+				<option value="neighborhood">Neighborhood</option>
+			</select></div>';
 	        $response['locality'] = '<div class="location_select_wrapper" style="display: none">' . pls_h( 'select', array( 'name' => 'location[locality]', 'id' => 'locality' ), pls_h_options( $form_options['locality'], wp_kses_post(@$_POST['location']['locality'] ), true )) . '</div>';
 	        $response['region'] = '<div class="location_select_wrapper" style="display: none">' . pls_h( 'select', array( 'name' => 'location[region]', 'id' => 'region' ), pls_h_options( $form_options['region'], wp_kses_post(@$_POST['location']['region'] ), true )) . '</div>';
 	        $response['postal'] = '<div class="location_select_wrapper" style="display: none">' . pls_h( 'select', array( 'name' => 'location[postal]', 'id' => 'postal' ), pls_h_options( $form_options['postal'], wp_kses_post(@$_POST['location']['postal'] ), true )) . '</div>';
 	        $response['neighborhood'] = '<div class="location_select_wrapper" style="display: none">' . pls_h( 'select', array( 'name' => 'location[neighborhood]', 'id' => 'neighborhood' ), pls_h_options( $form_options['neighborhood'], wp_kses_post(@$_POST['location']['neighborhood'] ), true )) . '</div>';
-	        $response['radius'] = '<select name="" id="" style="width: 140px">
-							<option value="100">100</option>
-							<option value="200">200</option>
-							<option value="300">300</option>
-							<option value="500">500</option>
-							<option value="700">700</option>
-							<option value="1000">1000</option>
-							<option value="1500">1500</option>
-							<option value="2000">2000</option>
-							<option value="5000">5000</option>
-						</select>';
 	        return $response;
 	}
 
@@ -487,11 +502,11 @@ class PLS_Map {
 				function pls_create_listing_marker (listing, map_js_var) {
 					var marker_details = {};
 					marker_details.latlng = new google.maps.LatLng(listing['location']['coords'][0], listing['location']['coords'][1]);
-					
-					if (listing['images'][0]['url']) {
+
+					if (listing['images'] && listing['images'][0] && listing['images'][0]['url']) {
 				    	var image_url = listing['images'][0]['url'];
 				    };
-
+				    console.log(listing);
 				    marker_details.content = '<div id="content">'+
                         '<div id="siteNotice">'+'</div>'+
                           '<h2 id="firstHeading" class="firstHeading">'+ listing['location']['full_address'] +'</h2>'+
@@ -573,6 +588,22 @@ class PLS_Map {
 				      }
 				    });
 				}
+
+				function pls_show_loading_overlay() {
+					jQuery('.map_wrapper #loading_overlay').show();
+				}
+
+				function pls_hide_loading_overlay() {
+					jQuery('.map_wrapper #loading_overlay').hide();	
+				}
+
+				function pls_show_empty_overlay() {
+					jQuery('.map_wrapper #empty_overlay').show();
+				}
+
+				function pls_hide_empty_overlay() {
+					jQuery('.map_wrapper #empty_overlay').hide();	
+				}
 				
 				
 			</script>
@@ -596,7 +627,11 @@ class PLS_Map {
         	'polygon_search' => false,
         	'life_style_search' => false,
         	'show_lifestyle_controls' => false,
-        	'show_lifestyle_checkboxes' => false
+        	'show_lifestyle_checkboxes' => false,
+        	'loading_overlay' => '<div>Loading...</div>',
+        	'empty_overlay' => '<div>No Results</div>',
+        	'search_on_load' => false,
+        	'polygon_options' => array()
         );
         $args = wp_parse_args( $args, $defaults );
         self::$map_js_var = $args['map_js_var'];	
