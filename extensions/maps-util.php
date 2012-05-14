50c1f775e37eba3f19195da4277c61e0bb532746
Galen Vinter <galen.vinter@gmail.com>
2012-05-14 13:22:25 -0400

    loading google maps manually in maps-util for listing function

--- extensions/maps-util.php
+++ extensions/maps-util.php
@@ -18,13 +18,14 @@ class PLS_Map {
 		self::make_markers($listings, $marker_args, $map_args);
 		extract($map_args, EXTR_SKIP);
 		
-		wp_enqueue_script('google-maps', 'http://maps.googleapis.com/maps/api/js?sensor=false&libraries=places');
+    // wp_enqueue_script('google-maps', 'http://maps.googleapis.com/maps/api/js?sensor=false&libraries=places');
 		wp_register_script('text-overlay', trailingslashit( PLS_JS_URL ) . 'libs/google-maps/text-overlay.js' );
 		wp_enqueue_script('text-overlay');
 
 
 		ob_start();
 		?>
+		<script src="http://maps.googleapis.com/maps/api/js?sensor=false&libraries=places" type="text/javascript"></script>
 			<script type="text/javascript">				
 				var <?php echo $map_js_var; ?> = {};
 				<?php echo $map_js_var; ?>.map;
