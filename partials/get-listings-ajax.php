dfa3f6ef4e0ba18cdd71a2565c2edde9f2f0985a
Galen Vinter <galen.vinter@gmail.com>
2012-07-07 11:12:47 -0400

    added back get-listings-ajax transient

--- partials/get-listings-ajax.php
+++ partials/get-listings-ajax.php
@@ -149,11 +149,11 @@ class PLS_Partials_Get_Listings_Ajax {
         $transient_id = 'pl_' . $signature;
         $transient = get_site_transient($transient_id);
         
-        // if ($transient) {
-        //     $transient['sEcho'] = $_POST['sEcho'];
-        //     echo json_encode($transient);
-        //     die();
-        // } 
+        if ($transient) {
+            $transient['sEcho'] = $_POST['sEcho'];
+            echo json_encode($transient);
+            die();
+        } 
 
         /** Extract the arguments after they merged with the defaults. */
         extract( wp_parse_args( $args, $defaults ), EXTR_SKIP );
