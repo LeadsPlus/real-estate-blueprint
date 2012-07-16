d76a2068e2ce39f05de4c0c65e55fdb257fc891f
Matt Barba <matt@placester.com>
2012-07-15 23:05:39 -0400

    cache the search form.
    Signed-off-by: Matt Barba <matt@placester.com>

--- partials/get-listings-search-form.php
+++ partials/get-listings-search-form.php
@@ -93,6 +93,10 @@ class PLS_Partials_Listing_Search_Form {
         );
 
         $args = wp_parse_args( $args, $defaults );
+        $cache = new PLS_Cache('Search Form');
+        if ($result = $cache->get($args)) {
+          return $result;
+        }
 
         $form_options = array();
 
@@ -709,6 +713,7 @@ class PLS_Partials_Listing_Search_Form {
         /** Filter the form. */
         $return = apply_filters( pls_get_merged_strings( array( "pls_listings_search_form_outer", $context ), '_', 'pre', false ), $form, $form_html, $form_options, $section_title, $form_data, $form_id, $context_var );
             
+        $cache->save($return);
         return $return;
 
 	}
